7370dab7e3dfdb7ea99fc3fdff7745918474375e
wzl361100698 <wzl361100698@users.noreply.github.com>
2026-07-30 10:17:25 +0800

    Fix Lifecycle compile SDK compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
